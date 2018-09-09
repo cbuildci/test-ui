d6893cb1b44fba7aa732a474e2cc076686721d85
Andre Mekkawi <github@andremekkawi.com>
2018-09-09 14:56:55 -0400

    Remove unused prop in CommitExecutionsPage container

--- app/containers/CommitExecutionsPage/index.js
+++ app/containers/CommitExecutionsPage/index.js
@@ -167,7 +167,6 @@ const mapDispatchToProps = (dispatch, ownProps) => bindActionCreators({
         ownProps.owner,
         ownProps.repo,
         ownProps.commit,
-        ownProps.executionNum,
     ),
     onClose: pageClosed,
 }, dispatch);
